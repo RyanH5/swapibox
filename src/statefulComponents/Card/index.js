d1e012b36dda465562cea94eaec67b541e314259
Ryan Harrington <Ryanaharrington5@gmail.com>
2018-07-06 17:59:23 -0600

    Populate cards with data

--- src/statefulComponents/Card/index.js
+++ src/statefulComponents/Card/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 export const Card = (props) => {
-  console.log(props)
   switch (props.currentCategory) {
     case 'people':
       return (
@@ -9,21 +8,28 @@ export const Card = (props) => {
           <h1>Name: {props.name}</h1>
           <h2>Homeworld: {props.homeworld}</h2>
           <h3>Species: {props.species}</h3>
-          <h5>Population: {props.population}</h5>
+          <h5>Homeworld Population: {props.population}</h5>
         </div>
       );
 
     case 'vehicles':
       return (
         <div>
-          <h1>{props.name}</h1>
+          <h1>Name: {props.name}</h1>
+          <h2>Model: {props.model}</h2>
+          <h3>Class: {props.vehicleClass}</h3>
+          <h5>Passengers: {props.passengers}</h5>
         </div>
       );
 
     case 'planets':
       return (
         <div>
-          <h1>{props.name}</h1>
+          <h1>Name: {props.name}</h1>
+          <h2>Terrain: {props.terrrain}</h2>
+          <h3>Climage: {props.climate}</h3>
+          <h4>Population: {props.population}</h4>
+          <h5>Residents: {props.residents}</h5>
         </div>
       );
   }
