f7c8ab916bb88fbd8ee0c0bb3b9aa8534278b68a
Ryan Harrington <Ryanaharrington5@gmail.com>
2018-07-06 18:53:39 -0600

    Add PropTypes validation to MainContainer and Card

--- src/statefulComponents/Card/index.js
+++ src/statefulComponents/Card/index.js
@@ -1,36 +1,64 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export const Card = (props) => {
-  switch (props.currentCategory) {
+  const {
+    currentCategory,
+    name,
+    homeworld,
+    species,
+    population,
+    model,
+    vehicleClass,
+    passengers,
+    terrain,
+    climate,
+    residents
+  } = props;
+  switch (currentCategory) {
     case 'people':
       return (
         <div>
-          <h1>Name: {props.name}</h1>
-          <h2>Homeworld: {props.homeworld}</h2>
-          <h3>Species: {props.species}</h3>
-          <h5>Homeworld Population: {props.population}</h5>
+          <h1>Name: {name}</h1>
+          <h2>Homeworld: {homeworld}</h2>
+          <h3>Species: {species}</h3>
+          <h5>Homeworld Population: {population}</h5>
         </div>
       );
 
     case 'vehicles':
       return (
         <div>
-          <h1>Name: {props.name}</h1>
-          <h2>Model: {props.model}</h2>
-          <h3>Class: {props.vehicleClass}</h3>
-          <h5>Passengers: {props.passengers}</h5>
+          <h1>Name: {name}</h1>
+          <h2>Model: {model}</h2>
+          <h3>Class: {vehicleClass}</h3>
+          <h5>Passengers: {passengers}</h5>
         </div>
       );
 
     case 'planets':
       return (
         <div>
-          <h1>Name: {props.name}</h1>
-          <h2>Terrain: {props.terrrain}</h2>
-          <h3>Climage: {props.climate}</h3>
-          <h4>Population: {props.population}</h4>
-          <h5>Residents: {props.residents}</h5>
+          <h1>Name: {name}</h1>
+          <h2>Terrain: {terrain}</h2>
+          <h3>Climage: {climate}</h3>
+          <h4>Population: {population}</h4>
+          <h5>Residents: {residents}</h5>
         </div>
       );
   }
+};
+
+Card.propTypes = {
+  currentCategory: PropTypes.string,
+  name: PropTypes.string,
+  homeworld: PropTypes.string,
+  species: PropTypes.string,
+  population: PropTypes.string,
+  model: PropTypes.string,
+  vehicleClass: PropTypes.string,
+  passengers: PropTypes.string,
+  terrain: PropTypes.string,
+  climate: PropTypes.string,
+  residents: PropTypes.array
 };
