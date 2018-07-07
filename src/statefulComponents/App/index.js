890595c29fdb31a021b2f2102462f9abd6c206ad
Ryan Harrington <Ryanaharrington5@gmail.com>
2018-07-06 18:08:09 -0600

    Remove redundant switch and unused imports in App

--- src/statefulComponents/App/index.js
+++ src/statefulComponents/App/index.js
@@ -7,16 +7,8 @@ import {
 } from 'react-router-dom';
 import Landing from '../Landing/index';
 import Navigation from '../../statelessComponents/Navigation/index';
-import { fetchPlanetData, fetchVehicles } from '../../helpers/fetch/index';
-import { getData } from '../../helpers/selectCategory/index';
-import {getCategory} from '../../helpers/selectCategory/index'
+import { getData, getCategory } from '../../helpers/selectCategory/index';
 import MainContainer from '../MainContainer/index';
-
-// import People from '../../statelessComponents/People/index';
-// import Vehicles from '../../statelessComponents/Vehicles/index';
-// import Planets from '../../statelessComponents/Planets/index';
-// import Favorites from '../../statelessComponents/Favorites/index';
-// import * as routes from '../../helpers/constants/routes';
 
 class App extends Component {
   constructor(props) {
@@ -40,25 +32,6 @@ class App extends Component {
       currentCategory: category
     });
   }
-
-  // displayCards = (category) => {
-  //   category.map(cat => {
-  //     console.log(cat)
-  //   });
-  // }
-  cardsSelection = () => {
-    const { people, planets, vehicles, favorites, currentCategory } = this.state;
-    switch (currentCategory) {
-      case 'people' || 'homeworld' || 'species': 
-        return people;
-      
-      case 'planets' || 'residents':
-        return planets;
-  
-      case 'vehicles':
-        return vehicles;     
-    }
-  };
 
   componentDidMount() {
   }
