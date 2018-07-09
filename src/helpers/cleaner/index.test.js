d19fb071c13d2e35bf2ccb059e75a5ccd8b91de8
Ryan Harrington <Ryanaharrington5@gmail.com>
2018-07-08 21:42:50 -0600

    Create mock data files

--- src/helpers/cleaner/index.test.js
+++ src/helpers/cleaner/index.test.js
@@ -1,1 +1,16 @@
-import 
+import { cleanFilmCrawlData } from './index';
+import { mockFilmData } from '../../mockData/mockFilmData';
+import { fetchStarWarsData } from '../fetch/index';
+
+describe('cleanFilmCrawlData', () => {
+  let mockUrl = "https://swapi.co/api/films";
+  it.skip('should return correct film data only', () => {
+    window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+      json: () => Promise.resolve(mockFilmData),
+      status: 200,
+      ok: true
+    }));
+    const uncleanData = fetchStarWarsData(mockUrl);
+    const actual = cleanFilmCrawlData(uncleanData);
+  });
+});
