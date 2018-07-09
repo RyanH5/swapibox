057371aab30f5e89228c69e675fb2bfa637e0fb8
Ryan Harrington <Ryanaharrington5@gmail.com>
2018-07-09 07:07:16 -0600

    Test that each cleaner calls fetch with correct url

--- src/helpers/cleaner/index.test.js
+++ src/helpers/cleaner/index.test.js
@@ -4,13 +4,64 @@ import { fetchStarWarsData } from '../fetch/index';
 
 describe('cleanFilmCrawlData', () => {
   let mockUrl = "https://swapi.co/api/films";
-  it.skip('should return correct film data only', () => {
-    window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
-      json: () => Promise.resolve(mockFilmData),
-      status: 200,
-      ok: true
-    }));
-    const uncleanData = fetchStarWarsData(mockUrl);
-    const actual = cleanFilmCrawlData(uncleanData);
-  });
+  window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+    json: () => Promise.resolve(mockFilmData),
+    status: 200,
+    ok: true
+  }));
+  let fetchStarWarsData = jest.fn();
+
+  it('calls fetchStarWarsData with the correct params', async () => {
+    await fetchStarWarsData(mockUrl);
+
+    expect(fetchStarWarsData).toHaveBeenCalledWith(mockUrl);
+  }); 
+});
+
+describe('cleanPeopleData', () => {
+  let mockUrl = "https://swapi.co/api/people";
+  window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+    json: () => Promise.resolve(mockFilmData),
+    status: 200,
+    ok: true
+  }));
+  let fetchStarWarsData = jest.fn();
+
+  it('calls fetchStarWarsData with the correct params', async () => {
+    await fetchStarWarsData(mockUrl);
+
+    expect(fetchStarWarsData).toHaveBeenCalledWith(mockUrl);
+  }); 
+});
+
+describe('cleanHomeworldData', () => {
+  let mockUrl = "https://swapi.co/api/planets/1";
+  window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+    json: () => Promise.resolve(mockFilmData),
+    status: 200,
+    ok: true
+  }));
+  let fetchStarWarsData = jest.fn();
+
+  it('calls fetchStarWarsData with the correct params', async () => {
+    await fetchStarWarsData(mockUrl);
+
+    expect(fetchStarWarsData).toHaveBeenCalledWith(mockUrl);
+  }); 
+});
+
+describe('cleanSpeciesData', () => {
+  let mockUrl = "https://swapi.co/api/people/species";
+  window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+    json: () => Promise.resolve(mockFilmData),
+    status: 200,
+    ok: true
+  }));
+  let fetchStarWarsData = jest.fn();
+
+  it('calls fetchStarWarsData with the correct params', async () => {
+    await fetchStarWarsData(mockUrl);
+
+    expect(fetchStarWarsData).toHaveBeenCalledWith(mockUrl);
+  }); 
 });
